366198d2861c3e4f6ba2410951c78a81aeff1f0a
lghfoo <1162820772@qq.com>
2019-12-12 22:21:33 +0800

    voronoi add order

--- Noder/src/applications/imager/noise/voronoi_noise_node.hpp
+++ Noder/src/applications/imager/noise/voronoi_noise_node.hpp
@@ -3,12 +3,14 @@
 #include"../..//..//applications/mather/data.hpp"
 #include"../data.hpp"
 #include<cmath>
+
 namespace Imager {
-	class VoronoiNoiseNode : public Noder::TNode<4, 1> {
+	class VoronoiNoiseNode : public Noder::TNode<5, 1> {
 	public:
 		enum {
 			CELLW_INPUT,		// int32_t
 			CELLH_INPUT,		// int32_t
+			ORDER_INPUT,		// int32_t
 			WIDTH_INPUT,		// int32_t
 			HEIGHT_INPUT		// int32_t
 		};
@@ -18,6 +20,7 @@ namespace Imager {
 		VoronoiNoiseNode(PObject arg = nullptr) {
 			this->GetInputPort(CELLW_INPUT)->FlushData(Pointer<Data>(new Mather::Number<int32_t>));
 			this->GetInputPort(CELLH_INPUT)->FlushData(Pointer<Data>(new Mather::Number<int32_t>));
+			this->GetInputPort(ORDER_INPUT)->FlushData(Pointer<Data>(new Mather::Number<int32_t>));
 			this->GetInputPort(WIDTH_INPUT)->FlushData(Pointer<Data>(new Mather::Number<int32_t>));
 			this->GetInputPort(HEIGHT_INPUT)->FlushData(Pointer<Data>(new Mather::Number<int32_t>));
 
@@ -26,26 +29,10 @@ namespace Imager {
 		virtual void ProcessData()override {
 			auto cell_w = this->GetInputPort(CELLW_INPUT)->GetData<Mather::Number<int32_t>>()->value;
 			auto cell_h = this->GetInputPort(CELLH_INPUT)->GetData<Mather::Number<int32_t>>()->value;
+			auto order = this->GetInputPort(ORDER_INPUT)->GetData<Mather::Number<int32_t>>()->value;
 			auto width = this->GetInputPort(WIDTH_INPUT)->GetData<Mather::Number<int32_t>>()->value;
 			auto height = this->GetInputPort(HEIGHT_INPUT)->GetData<Mather::Number<int32_t>>()->value;
-			if (cell_w <= 0 || cell_h <= 0 || width <= 0 || height <= 0)return;
-
-			output.SetSize(width, height);
-			for (int i = 0; i < width; i++) {
-				for (int j = 0; j < height; j++) {
-					float ratio = VoronoiVoise(float(i), float(j), cell_w, cell_h);
-					ratio = std::min(1.0f, std::max(ratio, 0.0f));
-					uint8_t value = (uint8_t)(255.0f * ratio);
-					
-					output.SetPixel(i, j, Pixel(value, value, value));
-				}
-			}
-
-			this->GetOutputPort(IMAGE_OUTPUT)->UpdateData(&output);
-		}
-		Image output;
-	private:
-		float VoronoiVoise(float x, float y, float cell_w, float cell_h) {
+			if (cell_w <= 0 || cell_h <= 0 || width <= 0 || height <= 0 || order < 0)return;
 			int min_i = -1, max_i = 1, min_j = -1, max_j = 1;
 			if (cell_w <= cell_h) {
 				min_j = -1;
@@ -61,7 +48,27 @@ namespace Imager {
 				min_j = min_i - count;
 				max_j = max_i + count;
 			}
+			int sample_num = (max_i - min_i + 1) * (max_j - min_j + 1);
+			if (order >= sample_num)return;
+			output.SetSize(width, height);
+			for (int i = 0; i < width; i++) {
+				for (int j = 0; j < height; j++) {
+					float ratio = VoronoiVoise(float(i), float(j), cell_w, cell_h, order,
+						min_i, max_i, min_j, max_j, sample_num);
+					ratio = std::min(1.0f, std::max(ratio, 0.0f));
+					uint8_t value = (uint8_t)(255.0f * ratio);
+					
+					output.SetPixel(i, j, Pixel(value, value, value));
+				}
+			}
 
+			this->GetOutputPort(IMAGE_OUTPUT)->UpdateData(&output);
+		}
+		Image output;
+	private:
+		float VoronoiVoise(float x, float y, float cell_w, float cell_h, float order, 
+			int min_i, int max_i, int min_j, int max_j, int sample_num) {
+			std::vector<float>samples(sample_num);
 			float cell_index_x = x / cell_w;
 			float cell_index_y = y / cell_h;
 			float max_dist = std::hypotf(float(cell_w), float(cell_h));
@@ -77,7 +84,9 @@ namespace Imager {
 			};
 			float base_cell_x = std::floorf(cell_index_x);
 			float base_cell_y = std::floorf(cell_index_y);
-			float min_dist = max_dist * 3;
+			//float min_dist = max_dist * 3;
+			
+			int count = 0;
 			for (int i = min_i; i <= max_i; i++) {
 				for (int j = min_j; j <= max_j; j++) {
 					float cell_x = (base_cell_x + i) * cell_w;
@@ -88,12 +97,14 @@ namespace Imager {
 					float cell_pos_y = cell_y + cell_random_y * cell_h;
 					// distance between CURRENT PIXEL and THE RANDOM POINT IN CELL[I, J]
 					float dist = std::hypotf(x - cell_pos_x, y - cell_pos_y);
-					if (dist < min_dist) {
-						min_dist = dist;
-					}
+					samples[count++] = dist;
+					//if (dist < min_dist) {
+					//	min_dist = dist;
+					//}
 				}
 			}
-			return min_dist / max_dist;
+			std::sort(samples.begin(), samples.end());
+			return samples[order] / max_dist;
 		}
 		//float VoronoiVoise(float x, float y, float cell_w, float cell_h) {
 		//	x /= cell_w;
